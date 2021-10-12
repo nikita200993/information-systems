15bf2915751ff85a49ca4b2d6d5e8a18229db6c7
NikitaRR <nikita200993@gmail.com>
2021-10-12 16:28:20 +0300

    fix path problem

--- practice-bash/git_remote_diff.sh
+++ practice-bash/git_remote_diff.sh
@@ -3,10 +3,14 @@
 url=$1
 branch1=$2
 branch2=$3
+work_dir=$(pwd)
+script_dir=$(dirname "$(realpath "$0")")
+project_dir="$script_dir/.project"
+diff_file="$script_dir/diff.txt"
 
-git clone -b $branch1 -- $url .project && \
-cd ./.project && \
-git fetch origin $branch2 && \
-git diff origin/$branch2 > ../diff.txt && \
-cd ../ && \
-rm -rf .project
+git clone -b "$branch1" -- "$url" "$project_dir" && \
+cd "$project_dir" && \
+git fetch origin "$branch2" && \
+git diff origin/"$branch2" > "$diff_file" && \
+cd "$work_dir" && \
+rm -rf "$project_dir"
